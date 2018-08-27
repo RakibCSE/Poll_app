2ae88d989275de244f8e0e9ab5592b4ebd5fb349
RakibCSE <rakibul.hossain365@gmail.com>
2018-08-27 16:13:48 +0600

    indexview test added

--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
 
 @python_2_unicode_compatible            # if you need to support python 2
 class Choice(models.Model):
@@ -23,3 +25,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+        
